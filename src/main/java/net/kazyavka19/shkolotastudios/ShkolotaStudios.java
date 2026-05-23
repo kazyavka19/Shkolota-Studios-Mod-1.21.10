6c614a21f95984bd702aa506a6d3ef1453786289
kazyavka19 <blaik.osa3400@gmail.com>
2026-05-24 01:25:01 +0300

    adding the cherry juice item yippi

--- src/main/java/net/kazyavka19/shkolotastudios/ShkolotaStudios.java
+++ src/main/java/net/kazyavka19/shkolotastudios/ShkolotaStudios.java
@@ -2,6 +2,7 @@ package net.kazyavka19.shkolotastudios;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kazyavka19.shkolotastudios.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class ShkolotaStudios implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/kazyavka19/shkolotastudios/item/ModItems.java
@@ -0,0 +1,37 @@
+package net.kazyavka19.shkolotastudios.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.kazyavka19.shkolotastudios.ShkolotaStudios;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static Item CHERRY_JUICE;
+
+    public static void registerModItems() {
+        ShkolotaStudios.LOGGER.info("Registering Mod Items for " + ShkolotaStudios.MOD_ID);
+
+        // 1. Create the Identifier
+        Identifier id = Identifier.of(ShkolotaStudios.MOD_ID, "cherry_juice");
+
+        // 2. Create the RegistryKey from the Identifier
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+
+        // 3. Register the item using the key, and bind that key into the Item.Settings
+        CHERRY_JUICE = Registry.register(
+                Registries.ITEM,
+                key,
+                new Item(new Item.Settings().registryKey(key))
+        );
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(CHERRY_JUICE);
+        });
+    }
+}
